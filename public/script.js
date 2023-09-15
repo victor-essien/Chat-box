dfb68768bae8251bc339b9146a458146d4f95098
Victor Essien <viktoressien1@gmail.com>
2023-09-15 01:05:35 +0100

    Made Changes and Fixed Error .  Used tailwindcss in the styling .  Fixed the 404 error,mime type error and api fetch error .  Used a different approach with the openai Api

--- public/script.js
+++ public/script.js
@@ -1,44 +1,130 @@
-const btn = document.getElementById("btn")
+const btn = document.getElementById("btn");
 
-btn.addEventListener('click', getResponse)
+async function handleSubmitMessage(message) {
+  if (!message) {
+    return alert("Please enter your question");
+  }
 
-async function getResponse(){
- let inputText = document.getElementById('input').value
- const parentDiv = document.getElementById('chat-area')
+  addUserMessageToCont(message);
+  //  let inputText = document.getElementById('input').value
+  //
 
- 
-    const question = document.createElement('div')
-    question.innerHTML = inputText
-    question.classList.add('box')
-    parentDiv.appendChild(question)
-    document.getElementById('input').value =""
-    try{
-    let res = await fetch('http://localhost:5000/chat', 
-  {
-    method: 'POST',
+  let res = await fetch("/api/openai", {
+    method: "POST",
     headers: {
-      "Content-Type": 'application/json'                
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      question: inputText          
-    })
-  }
-)
+    body: JSON.stringify({ message }),
+  });
 
-const data = await res.json() 
+  const data = await res.json();
 
-if(data.message) {
-    const answer = document.createElement('div')
-    answer.innerHTML = data.message.content
-    answer.classList.add("box", "answer")
-    parentDiv.appendChild(answer)
-  }
-}catch(error){
-    console.log("Error:", error)
+  return data;
 }
-  if(inputText === ''){
-    
- }
- }
 
- 
+function addUserMessageToCont(message) {
+  const userMessage = document.createElement("div");
+  const parentDiv = document.getElementById("chat-area");
+  userMessage.classList.add(
+    "bg-indigo-500",
+    "text-white",
+    "rounded",
+    "p-2",
+    "w-fit",
+    "self-end",
+    "break-words",
+    "overflow-auto"
+  );
+  userMessage.innerText = message;
+  parentDiv.appendChild(userMessage);
+  document.getElementById("input").value = "";
+  addLoadingIndicatorToDialogueBox();
+}
+// add the loading indicator to the dialogue box
+function addLoadingIndicatorToDialogueBox() {
+  const parentDiv = document.getElementById("chat-area");
+  // create a new li element
+  const loadingIndicator = document.createElement("div");
+
+  // set the id of the loading indicator
+  loadingIndicator.id = "loading-indicator";
+
+  // add loading indicator styling
+  loadingIndicator.classList.add(
+    "bg-gray-500",
+    "text-white",
+    "rounded",
+    "p-1",
+    "w-fit",
+    "self-start",
+    "w-12"
+  );
+
+  // create a new image element
+  const loadingImage = document.createElement("img");
+
+  // set the image source
+  loadingImage.src = "loading.svg";
+
+  // add loading indicator image as a child to li element
+  loadingIndicator.appendChild(loadingImage);
+
+  // add the li element to the DOM
+  parentDiv.appendChild(loadingIndicator);
+}
+
+// remove the loading indicator from the dialogue box
+function removeLoadingIndicatorFromDialogueBox() {
+  // get the loading indicator element
+  const loadingIndicator = document.getElementById("loading-indicator");
+
+  // remove the loading indicator from the DOM
+  loadingIndicator.remove();
+}
+
+function addBotMessageToCont(response) {
+  removeLoadingIndicatorFromDialogueBox();
+  const botMessage = document.createElement("div");
+  const parentDiv = document.getElementById("chat-area");
+  if (response.status === "error") {
+    // add error styling
+    botMessage.classList.add(
+      "bg-red-500",
+      "text-white",
+      "rounded",
+      "p-2",
+      "w-fit",
+      "self-start"
+    );
+
+    // add error text
+    botMessage.innerText =
+      "Oh no! Something went wrong. Please try again later.";
+  } else {
+    botMessage.classList.add(
+      "bg-gray-500",
+      "text-white",
+      "rounded",
+      "p-2",
+      "w-fit",
+      "self-start"
+    );
+    botMessage.innerText = response.data.trim();
+    parentDiv.appendChild(botMessage);
+    document.getElementById("input").value = "";
+  }
+}
+
+window.onload = () =>
+  document.getElementById("btn").addEventListener("click", (e) => {
+    // prevent the form from refreshing the page
+
+    // get the value of the input
+    const message = document.getElementById("input").value;
+
+    // call the function that handles the fetch request to our backend
+    handleSubmitMessage(message).then((data) => {
+      // add the chatbot's response to the DOM when the fetch request is complete
+      addBotMessageToCont(data);
+    });
+  });
--- server.js
+++ server.js
@@ -1,36 +1,93 @@
-import express  from 'express'
-import  OpenAI  from 'openai'
-
+import express from "express";
+import OpenAI from "openai";
+import fetch from "node-fetch";
+import bodyParser from "body-parser";
 import { config } from "dotenv";
+import path from "path";
 
+// Serve static files from the 'public' directory
 
-const app = express()
-app.use(express.static('public')) 
-app.use(express.json())
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+const app = express();
+
+//app.use(express.static('public'))
+//app.use(express.static(path.join(process.cwd(), 'public'), { 'extensions': ['css'] }));
+//app.use(express.static('public'))
+app.use(express.static(path.join(process.cwd(), "public")));
+//app.use(express.static('public'));
+//app.use(express.static(__dirname + '/public'));
+
+app.use(express.json());
 config();
-const PORT = process.env.PORT || 3030
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 3030;
 const API_KEY = process.env.API_KEY;
-const openai = new OpenAI ({
-    apiKey :API_KEY
-})
 
-app.post('/chat', async (req, res)=> {   
-   
-    const requestData = req.body.question
-    try {
-      const resp = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-          messages: [
-            { "role": "user", content: requestData}
-          ]  
-      })           
-          
-      res.status(200).json({message: resp.choices[0].message})
-    } catch(e) {
-        res.status(400).json({message: e.message})
-    }
-  })
-  
+global.messages = [
+  {
+    role: "system",
+    content:
+      "You are a helpful, empathetic, and friendly customer support specialist. You are here to help customers with their orders. You sometimes make small talk.",
+  },
+  {
+    role: "system",
+    content:
+      "Additionally, you never ask the customer to upload or provide any photos as our website has no means of doing so at this time. Also, do not mention that you are a bot.",
+  },
+];
+app.post("/api/openai", async (req, res) => {
+  const { message } = req.body;
+
+  // store user message in global message state
+  const userMessage = { role: "user", content: message };
+
+  // add to global messages list
+  global.messages.push(userMessage);
+
+  const resp = await fetch("https://api.openai.com/v1/chat/completions", {
+    // model: "gpt-3.5-turbo",
+    //   messages: [
+    //     { "role": "user", content: requestData}
+    //   ]
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: global.messages,
+      // the maximum number of tokens/words the bot should return
+      // in response to a given prompt
+      max_tokens: 150,
+    }),
+  });
+
+  if (!resp.ok) {
+    // if the request was not successful, parse the error
+    const error = await resp.json();
+
+    // log the error for debugging purposes
+    console.error("OpenAI API Error:", error);
+
+    // return an error response to the client
+    return res.json({ status: "error", data: null });
+  }
+  // parse the response from OpenAI as json
+  const data = await resp.json();
+  // get the bot's answer from the OpenAI API response
+  const botAnswer = data?.choices?.[0]?.message?.content;
+
+  // create the bot message object
+  const botMessage = { role: "assistant", content: botAnswer };
+
+  // store bot message in global message state
+  global.messages.push(botMessage);
+
+  return res.json({ status: "success", data: botAnswer });
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
